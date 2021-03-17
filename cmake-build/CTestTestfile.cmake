ea37f6cbb9373dba26e4727a39f8f99f4bb8b5c7
Rudukh <Rudukh.Eugene@yandex.ru>
2021-03-18 00:01:30 +0300

    delete cmake-build and add gitignire

--- cmake-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lyalyashechka/TP/c_c-_tp
-# Build directory: /home/lyalyashechka/TP/c_c-_tp/cmake-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
